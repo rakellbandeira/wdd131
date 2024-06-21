2f569abbe98cb7c5e5f3750beaffe2d8488e277c
rakellbandeira <per22016l@byui.edu>
2024-06-21 19:22:12 -0400

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+let currentYear = document.getElementById('currentYear');
+currentYear.innerHTML = '2024';
+
+let dateMod = new Date(document.lastModified);
+let date = dateMod.getDate();
+let month = dateMod.getMonth();
+let year = dateMod.getFullYear();
+let hour = dateMod.getHours();
+let min = dateMod.getMinutes();
+
+let lastModified = document.getElementById('lastModified');
+lastModified.innerHTML = `Last Modified: ${date}/${month}/${year} ${hour}:${min}`;
