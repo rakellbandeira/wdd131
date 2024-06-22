09479b95f48cc47eaed1dddad7af612dcaed4081
rakellbandeira <per22016l@byui.edu>
2024-06-22 17:17:14 -0400

    Week 02 - Event Handling

--- week02/scripts/bom-script.js
+++ week02/scripts/bom-script.js
@@ -1,13 +1,39 @@
-const input = document.querySelector('#favchap');
+const input = document.getElementById('favchap');
+/* querySelector('#favchap'); */
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const liItem = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-liItem.textContent = input.value;
-deleteButton.textContent = '&#x274C';
 
-liItem.append(deleteButton);
+/* Create a click event listener for the Add Chapter button using an addEventListener. */
+button.addEventListener('click', function() {
+    if (input.value.trim() !== ''){
+        const liItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-list.append(liItem);
+        liItem.textContent = input.value;
+        deleteButton.innerHTML = '&#x274C';
+
+        liItem.append(deleteButton);
+
+        list.append(liItem);
+        input.value = '';
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(liItem);
+            input.value = '';
+            input.focus();
+        
+        });
+    
+    }
+    else{        
+        input.focus();
+    }
+
+    
+});
+
+
+
+input.focus();
